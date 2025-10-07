015e3dc0cf4d800746b29a72e0302af2e90b1c90
Lungsi <neuralgraphs@gmail.com>
2025-10-07 16:27:53 +0200

    created popactivity

--- src/analyseur/cbgt/visual/histo.py
+++ src/analyseur/cbgt/visual/histo.py
@@ -30,6 +30,7 @@ def psth(spiketrains, binsz=50, window=(0,10000), nucleus=None):
     
     * `window` controls the binning range as well as the spike counting window
     * CBGT simulation was done in milliseconds so window `(0, 10000)` signifies time 0 ms to 10,000 ms (or 10 s)
+    * `psth` gives an overall temporal pattern of population activity
     
     **Use Case:**
 
@@ -55,9 +56,10 @@ def psth(spiketrains, binsz=50, window=(0,10000), nucleus=None):
 
       psth(spike_trains, window=(0,50), binsz=1)
       psth(spike_trains, window=(0,50), binsz=0.05)
-    
+
+
     """
-    [desired_spiketrains, yticks] = get_desired_spiketrains(spiketrains)
+    [desired_spiketrains, _] = get_desired_spiketrains(spiketrains)
 
     bins = np.arange(window[0], window[1] + binsz, binsz)
     allspikes = np.concatenate(desired_spiketrains)
@@ -92,6 +94,7 @@ def psrh(spiketrains, binsz=10, window=(0,10000), nucleus=None):
     
     * `window` controls the binning range as well as the spike counting window
     * CBGT simulation was done in milliseconds so window `(0, 10000)` signifies time 0 ms to 10,000 ms (or 10 s)
+    * `psrh` gives a collective dynamics of the population ensemble
     
     **Use Case:**
 
@@ -118,18 +121,21 @@ def psrh(spiketrains, binsz=10, window=(0,10000), nucleus=None):
       psrh(spike_trains, window=(0,50), binsz=1)
       psrh(spike_trains, window=(0,50), binsz=0.05)
 
+
     """
-    [desired_spiketrains, yticks] = get_desired_spiketrains(spiketrains)
+    [desired_spiketrains, _] = get_desired_spiketrains(spiketrains)
     n_neurons = len(desired_spiketrains)
 
     bins = np.arange(window[0], window[1] + binsz, binsz)
     t_axis = bins[:-1] + binsz / 2
 
+    # Population Rate Histogram
     pop_rate = np.zeros(len(bins) - 1)
     for spikes in desired_spiketrains:
         counts, _ = np.histogram(spikes, bins=bins)
         pop_rate += counts
-    pop_rate = 1000*(pop_rate / (binsz * n_neurons)) # spikes per milliseconds neurons
+    # Histogram to firing rate
+    pop_rate = 1000*(pop_rate / (binsz * n_neurons)) # in seconds [default: spikes per milliseconds neurons]
 
     plt.plot(t_axis, pop_rate, linewidth=2)
     plt.fill_between(t_axis, pop_rate, alpha=0.3)
@@ -139,8 +145,143 @@ def psrh(spiketrains, binsz=10, window=(0,10000), nucleus=None):
     plt.xlabel("Time (ms)")
 
     nucname = "" if nucleus is None else " in " + nucleus
-    plt.title("Population firing rate of " + str(n_neurons) + " neurons" + nucname)
+    plt.title("Population Spiking Rate Histogram of " + str(n_neurons) + " neurons" + nucname)
 
     plt.show()
 
     return plt
+
+def popactivity(spiketrains, binsz=10, window=(0,10000), nucleus=None):
+    """
+    Displays the Population Activity Heatmap of the given spike times and returns the plot figure (to save if necessary).
+
+    :param spiketrains: Dictionary returned using :class:`~analyseur/cbgt/loader.LoadSpikeTimes`
+    :param binsz: defines the number of equal-width bins in the range [default: 50]
+    :param window: defines upper and lower range of the bins but ignore lower and upper outliers [default: (0,10000)]
+    :param nucleus: [OPTIONAL] None or name of the nucleus (string)
+    :return: object `matplotlib.pyplot.imshow <https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.imshow.html>`_
+    
+    * `window` controls the binning range as well as the spike counting window
+    * CBGT simulation was done in milliseconds so window `(0, 10000)` signifies time 0 ms to 10,000 ms (or 10 s)
+    * `popactivity` gives a spatio-temporal pattern across neurons
+    
+    **Use Case:**
+
+    1. Setup
+
+    ::
+
+      from  analyseur.cbgt.loader import LoadSpikeTimes
+      loadST = LoadSpikeTimes("/full/path/to/spikes_GPi.csv")
+      spike_trains = loadST.get_spiketrains()
+
+      from analyseur.cbgt.visual.histo import popactivity
+
+    2. Population Activity Heatmap for the entire simulation window
+
+    ::
+
+      popactivity(spike_trains)
+
+    3. Population Activity Heatmap for desired window and bin size
+
+    ::
+
+      popactivity(spike_trains, window=(0,50), binsz=1)
+      popactivity(spike_trains, window=(0,50), binsz=0.05)
+
+
+    """
+    [desired_spiketrains, _] = get_desired_spiketrains(spiketrains)
+    n_neurons = len(desired_spiketrains)
+
+    bins = np.arange(window[0], window[1] + binsz, binsz)
+
+    # Activity Matrix
+    activity = np.zeros((n_neurons, len(bins) - 1))
+    for i, spikes in enumerate(desired_spiketrains):
+        counts, _ = np.histogram(spikes, bins=bins)
+        activity[i] = counts
+    activity = activity[::-1, :] # reverse it so that neuron 0 is at the bottom
+
+    plt.imshow(activity, aspect="auto", cmap="hot",
+               # extent=[window[0], window[1], n_neurons, 0] # if neuron 0 is at the top by default
+               extent = [window[0], window[1], 0, n_neurons])
+    plt.colorbar(label="Spike Count per Bin")
+
+    plt.ylabel("neurons")
+    plt.xlabel("Time (ms)")
+
+    nucname = "" if nucleus is None else " in " + nucleus
+    plt.title("Population Activity Heatmap of " + str(n_neurons) + " neurons" + nucname)
+
+    plt.show()
+
+def popfr_variability(spiketrains, binsz=10, window=(0,10000), nucleus=None):
+    """
+    Displays the Population Activity Heatmap of the given spike times and returns the plot figure (to save if necessary).
+
+    :param spiketrains: Dictionary returned using :class:`~analyseur/cbgt/loader.LoadSpikeTimes`
+    :param binsz: defines the number of equal-width bins in the range [default: 50]
+    :param window: defines upper and lower range of the bins but ignore lower and upper outliers [default: (0,10000)]
+    :param nucleus: [OPTIONAL] None or name of the nucleus (string)
+    :return: object `matplotlib.pyplot.imshow <https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.imshow.html>`_
+    
+    * `window` controls the binning range as well as the spike counting window
+    * CBGT simulation was done in milliseconds so window `(0, 10000)` signifies time 0 ms to 10,000 ms (or 10 s)
+    * `popfr_variability` gives heterogeneity in population responses
+    
+    **Use Case:**
+
+    1. Setup
+
+    ::
+
+      from  analyseur.cbgt.loader import LoadSpikeTimes
+      loadST = LoadSpikeTimes("/full/path/to/spikes_GPi.csv")
+      spike_trains = loadST.get_spiketrains()
+
+      from analyseur.cbgt.visual.histo import popactivity
+
+    2. Population Activity Heatmap for the entire simulation window
+
+    ::
+
+      popactivity(spike_trains)
+
+    3. Population Activity Heatmap for desired window and bin size
+
+    ::
+
+      popactivity(spike_trains, window=(0,50), binsz=1)
+      popactivity(spike_trains, window=(0,50), binsz=0.05)
+
+
+    """
+    [desired_spiketrains, _] = get_desired_spiketrains(spiketrains)
+    n_neurons = len(desired_spiketrains)
+
+    bins = np.arange(window[0], window[1] + binsz, binsz)
+    t_axis = bins[:-1] + binsz / 2
+
+    # Per bin compute firing rate per neuron
+    fr = np.zeros((n_neurons, len(bins)-1))
+    for i, spikes in enumerate(desired_spiketrains):
+        counts, _ = np.histogram(spikes, bins=bins)
+        fr[i] = 1000*(counts / binsz) # to seconds [default: spikes / milliseconds]
+
+    mean_fr = np.mean(fr, axis=0)
+    std_fr = np.std(fr, axis=0)
+
+    plt.plot(t_axis, mean_fr, label="Mean", linewidth=2)
+    plt.fill_between(t_axis, mean_fr - std_fr, mean_fr + std_fr,
+                     alpha=0.3, label="±1 STD")
+    plt.grid(True, alpha=0.3)
+
+    plt.ylabel("Firing Rate (Hz)")
+    plt.xlabel("Time (ms)")
+
+    nucname = "" if nucleus is None else " in " + nucleus
+    plt.title("Population Firing Rate (Mean ± STD) Variability of " + str(n_neurons) + " neurons" + nucname)
+
+    plt.show()
