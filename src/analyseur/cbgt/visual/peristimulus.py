924568ddd69e053556bcc1c0aaa225fb83c547d1
Lungsi <neuralgraphs@gmail.com>
2025-10-09 14:21:56 +0200

    fixed and commented PSTH methods

--- src/analyseur/cbgt/visual/peristimulus.py
+++ src/analyseur/cbgt/visual/peristimulus.py
@@ -7,7 +7,6 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import stats
 
 from ..loader import get_desired_spiketrains
 
@@ -69,47 +68,79 @@ class PSTH(object):
     def __init__(self, spiketrains):
         self.spiketrains = spiketrains
 
-    def _compute_firing_rate_in_window(self, window, allspikes_in_window):
-        total_time = window[1] - window[0]
-        total_spikes = len(allspikes_in_window)
-        # return 1000 * (total_spikes / total_time)  # in hertz
-        return (total_spikes / total_time)  # in kHz
+    def _compute_true_avg_firing_rate(self, window, allspikes_in_window):
+        """
+        Computes the average of each neuron's firing rate over the entire period
 
-    def _compute_psth(self, binsz=50, window=(0, 10000)):
-        [self.desired_spiketrains, _] = get_desired_spiketrains(self.spiketrains)
+        :param window:
+        :param allspikes_in_window:
+        :return: dictionary with keys: firing_rates, mean_firing_rate, std_firing_rate
+        """
+        firing_rates = []
+        total_duration = window[1] - window[0]
 
-        allspikes = np.concatenate(self.desired_spiketrains)
+        for indiv_spiketimes in allspikes_in_window:
+            spiketimes = np.array(indiv_spiketimes)
+            spikes_in_window = spiketimes[(spiketimes >= window[0]) & (spiketimes <= window[1])]
+            indiv_rate = len(spikes_in_window) / total_duration # kHz
+            firing_rates.append(indiv_rate)
+
+        return {
+            "firing_rates": np.array(firing_rates),
+            "mean_firing_rate": np.mean(firing_rates),
+            "std_firing_rate": np.std(firing_rates),
+        }
+
+    def _compute_pop_firing_rate_in_window(self, binsz, allspikes_in_window, pop_counts):
+        total_neurons = len(allspikes_in_window)
+        return pop_counts / (total_neurons * binsz)  # in kHz
+
+    def _compute_psth(self, desired_spiketrains, binsz=50, window=(0, 10000)):
+        allspikes = np.concatenate(desired_spiketrains)
         allspikes_in_window = allspikes[(allspikes >= window[0]) &
                                         (allspikes <= window[1])]  # Memory efficient
 
         bins = np.arange(window[0], window[1] + binsz, binsz)
+        # bin_centers = (bins[:-1] + bins[1:]) / 2
 
         # Compute
         counts, bin_edges = np.histogram(allspikes_in_window, bins=bins)
 
         bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
 
-        fr = self._compute_firing_rate_in_window(window, allspikes_in_window)
+        # firerate = self._compute_firing_rate_in_window(window, allspikes_in_window)
+        popfirerates = self._compute_pop_firing_rate_in_window(binsz, allspikes_in_window, counts)
+        true_avg_rate = self._compute_true_avg_firing_rate(window, allspikes_in_window)
 
-        return counts, bin_centers, binsz, fr
+        return counts, bin_centers, popfirerates, true_avg_rate, allspikes_in_window
 
     def plot(self, binsz=50, window=(0, 10000), nucleus=None):
         """
         Displays the Peri-Stimulus Time Histogram (PSTH) of the given spike times
         and returns the plot figure (to save if necessary).
         
-        :param binsz: defines the number of equal-width bins in the range [default: 50]
-        :param window: defines upper and lower range of the bins but ignore lower and upper outliers [default: (0,10000)] 
-        :param nucleus: [OPTIONAL] None or name of the nucleus (string) 
+        :param binsz: integer or float; defines the number of equal-width bins in the range [default: 50]
+        :param window: 2-tuple; defines upper and lower range of the bins but ignore lower and upper outliers [default: (0,10000)]
+        :param nucleus: string; [OPTIONAL] None or name of the nucleus
         :return: object `matplotlib.pyplot.hist <https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.hist.html>`_
         
         * `window` controls the binning range as well as the spike counting window
         * CBGT simulation was done in milliseconds so window `(0, 10000)` signifies time 0 ms to 10,000 ms (or 10 s)
         
         """
-        # Compute PSTH and set the results as class attributes
-        [self.counts, self.bin_centers, self.binsz, self.fr] = \
-            self._compute_psth(binsz=binsz, window=window)
+        # Set binsz and window as the instance attributes
+        self.binsz = binsz
+        self.window = window
+
+        # Get and set desired_spiketrains as instance attribute
+        [self.desired_spiketrains, _] = get_desired_spiketrains(self.spiketrains)
+        # NOTE: desired_spiketrains as nested list and not numpy array because
+        # each neuron may have variable length of spike times
+
+        # Compute PSTH and set the results as instance attributes
+        [self.counts, self.bin_centers, self.popfirerates,
+         self.true_avg_rate, self.allspikes_in_window] = \
+            self._compute_psth(self.desired_spiketrains, binsz=binsz, window=window)
 
         # Plot
         plt.bar(self.bin_centers, self.counts, width=binsz,
@@ -122,7 +153,8 @@ class PSTH(object):
         nucname = "" if nucleus is None else " in " + nucleus
         allno = str(len(self.desired_spiketrains))
         plt.title("PSTH - Population Activity of " + allno + " neurons" + nucname +
-                  "\n (firing rate within the window = " + str(self.fr) + " kHz)")
+                  "\n (mean firing rate within the window = "
+                  + str(self.true_avg_rate["mean_firing_rate"]) + " kHz)")
 
         plt.show()
         
@@ -134,55 +166,81 @@ class PSTH(object):
         :param stimulus_onset: [OPTIONAL] default: 0
         :return: dictionary
         
-        +------------------------+----------------------+
-        | Dictionary key         | Meaning              |
-        +========================+======================+
-        | `"response_latency"`   | when response begins |
-        +------------------------+----------------------+
-        | `"response_magnitude"` | peak amount          |
-        +------------------------+----------------------+
-        | `"peak_latency"`       | when peak occurs     |
-        +------------------------+----------------------+
-        | `"response_duration"`  | how long it lasts    |
-        +------------------------+----------------------+
-        | `"temporal_profile"`   | complete time course |
-        +------------------------+----------------------+
-        | `"adaptation_ratio"`   |                      |
-        +------------------------+----------------------+
-        
+        +---------------------------+------------------------------+
+        | Dictionary key            | Meaning                      |
+        +===========================+==============================+
+        | `"peak_latency"`          | when peak occurs             |
+        +---------------------------+------------------------------+
+        | `"response_latencies"`    | when responses begins        |
+        +---------------------------+------------------------------+
+        | `"response_duration"`     | how long it lasts            |
+        +---------------------------+------------------------------+
+        | `"response_sequence"`     | time span of first responses |
+        +---------------------------+------------------------------+
+        | `"response_spread"`       | how synced are the neurons   |
+        +---------------------------+------------------------------+
+        | `"temporal_coordination"` | coordination index           |
+        +---------------------------+------------------------------+
+
+        * the temporal profile (full time course) is the value of the attribute :ivar popfirerates:
+
         """
-        # When does the neuron respond
-        response_threshold = \
-            np.mean(self.counts[self.bin_centers < stimulus_onset]) + 2 * np.std( self.counts)
+        pre_stimulus_rates = self.popfirerates[self.bin_centers < stimulus_onset]
+        post_stimulus_rates = self.popfirerates[self.bin_centers >= stimulus_onset]
+        post_stimulus_times = self.bin_centers[self.bin_centers >= stimulus_onset]
 
-        supra_threshold = self.counts[self.bin_centers >= stimulus_onset] > response_threshold
-
-        if np.any(supra_threshold):
-            response_latency = \
-                self.bin_centers[self.bin_centers >= stimulus_onset][np.where(supra_threshold)[0][0]] \
-                - stimulus_onset
+        # When does the neuron population respond
+        if not pre_stimulus_rates.size:
+            baseline = 0
+            response_threshold = 0
         else:
-            response_latency = None
-        
-        # Temporal pattern or response evolution
-        peak_time = self.bin_centers[np.argmax(self.counts)]
-        response_duration = len(self.counts[self.bin_centers >= stimulus_onset]) \
-                            * (self.bin_centers[1] - self.bin_centers[0])
-        
-        # Response dynamics
-        early_response = np.mean(self.counts[(self.bin_centers >= stimulus_onset) &
-                                             (self.bin_centers < stimulus_onset + 0.1)])
-        late_response = np.mean(self.counts[(self.bin_centers >= stimulus_onset + 0.1) &
-                                            (self.bin_centers < stimulus_onset + 0.5)])
+            baseline = np.mean(pre_stimulus_rates)
+            response_threshold = baseline + 2 * np.std(pre_stimulus_rates)
+
+        if len(post_stimulus_rates) > 0:
+            supra_thresholds = post_stimulus_rates > response_threshold
+            if np.any(supra_thresholds):
+                response_latencies = post_stimulus_times[np.where(supra_thresholds)[0][0]] - stimulus_onset
+            else:
+                response_latencies = None
+        else:
+            response_latencies = None
+
+        # Population response
+        peak_time = self.bin_centers[np.argmax(self.popfirerates)]
+
+        if response_latencies is not None:
+            response_periods = post_stimulus_rates > baseline
+            if np.any(response_periods):
+                response_duration = np.sum(response_periods) * self.binsz
+            else:
+                response_duration = np.array(0)
+        else:
+            response_duration = np.array(0)
+
+        # Response dynamics: Temporal pattern (response evolution) and
+        # temporal coordination among neurons
+        first_spike_times = [np.min(np.array(indiv_spiketimes)[np.array(indiv_spiketimes) >= stimulus_onset])
+                             if np.any(np.array(indiv_spiketimes) >= stimulus_onset)
+                             else np.inf
+                             for indiv_spiketimes in self.desired_spiketrains]
+        first_spike_times = [t for t in first_spike_times if t != np.inf]
+
+        if len(first_spike_times) > 0:
+            response_spread = np.std(first_spike_times) # spread of first spike times
+            response_sequence = np.max(first_spike_times) - np.min(first_spike_times) # total span
+        else:
+            response_spread = np.array(0)
+            response_sequence = np.array(0)
         
         return {
-            "response_latency": response_latency,
-            "response_magnitude": np.max(self.counts) -
-                                  np.mean(self.counts[self.bin_centers < stimulus_onset]),
-            "peak_latency": peak_time - stimulus_onset,
-            "response_duration": response_duration,
-            "temporal_profile": self.counts,
-            "adaptation_ratio": late_response /early_response if early_response > 0 else 0,
+            "peak_latency": peak_time.item() - stimulus_onset,
+            "response_latencies": response_latencies,
+            "response_duration": response_duration.item(),
+            "response_sequence": response_sequence.item(),
+            "response_spread": response_spread.item(),
+            "temporal_coordination": 1 / (response_spread.item() + 1e-8),
+            # "response_profile": self.popfirerates,
         }
 
     def analytics_rate(self, stimulus_onset=0):
@@ -191,48 +249,75 @@ class PSTH(object):
         :param stimulus_onset: [OPTIONAL] default: 0
         :return: dictionary
         
-        +--------------------------+---------------------------------+
-        | Dictionary key           | Meaning                         |
-        +==========================+=================================+
-        | `"baseline_rate"`        | spontaneous activity level      |
-        +--------------------------+---------------------------------+
-        | `"peak_rate"`            | maximum response magnitude      |
-        +--------------------------+---------------------------------+
-        | `"mean_response_rate"`   | average response strength       |
-        +--------------------------+---------------------------------+
-        | `"rate_increase"`        | absolute response magnitude     |
-        +--------------------------+---------------------------------+
-        | `"fold_change"`          | relative response magnitude     |
-        +--------------------------+---------------------------------+
-        | `"response_reliability"` | measure of response consistency |
-        +--------------------------+---------------------------------+
-        | `"response_entropy"`     | complexity of rate pattern      |
-        +--------------------------+---------------------------------+
-        
+        +--------------------------+------------------------------------------------------------+
+        | Dictionary key           | Meaning                                                    |
+        +==========================+============================================================+
+        | `"mean_firing_rate"`     | average of each neuron's rate                              |
+        +--------------------------+------------------------------------------------------------+
+        | `"std_firing_rate"`      | spread of the firing rates                                 |
+        +--------------------------+------------------------------------------------------------+
+        | `"avg_time_vary_rate"`   | average of the time varying population signal              |
+        +--------------------------+------------------------------------------------------------+
+        | `"mean_baseline_rate"`   | average spontaneous activity levels                        |
+        +--------------------------+------------------------------------------------------------+
+        | `"mean_response_rate"`   | average response strengths                                 |
+        +--------------------------+------------------------------------------------------------+
+        | `"mean_rate_increase"`   | average absolute response magnitudes                       |
+        +--------------------------+------------------------------------------------------------+
+        | `"mean_fold_change"`     | average relative response magnitudes                       |
+        +--------------------------+------------------------------------------------------------+
+        | `"active_fraction"`      | active neurons out of total neurons                        |
+        +--------------------------+------------------------------------------------------------+
+        | `"population_sparsity"`  | measure of concentration of firing rates across population |
+        +--------------------------+------------------------------------------------------------+
+        | `"rate_heterogeneity"`   | measure of response heterogeneity                          |
+        +--------------------------+------------------------------------------------------------+
+        | `"response_reliability"` | measure of response consistency                            |
+        +--------------------------+------------------------------------------------------------+
+
         """
-        pre_stimulus = self.counts[self.bin_centers < stimulus_onset]
-        post_stimulus = self.counts[self.bin_centers >= stimulus_onset]
-        
-        baseline_rate = np.mean(pre_stimulus)
-        response_peak = np.max(post_stimulus)
-        mean_response = np.mean(post_stimulus)
+        mean_firing_rate = self.true_avg_rate["mean_firing_rate"]
+        std_firing_rate = self.true_avg_rate["std_firing_rate"]
+
+        baseline_rates = []
+        response_rates = []
+
+        for indiv_spiketimes in self.allspikes_in_window:
+            spiketimes = np.array(indiv_spiketimes)
+
+            # Rates: Baseline vs Response
+            baseline_spikes = spiketimes[(spiketimes >= self.window[0]) & (spiketimes < stimulus_onset)]
+            response_spikes = spiketimes[spiketimes >= stimulus_onset]
+
+            baseline_duration = stimulus_onset - self.window[0]
+            response_duration = self.window[1] - stimulus_onset
+
+            baseline_rates.append( len(baseline_spikes) / baseline_duration
+                                   if baseline_duration > 0 else 0 )
+            response_rates.append( len(response_spikes) / response_duration
+                                   if response_duration > 0 else 0 )
+
+        baseline_rates = np.array(baseline_rates)
+        response_rates = np.array(response_rates)
         
         # Rate change metrics
-        rate_increase = response_peak - baseline_rate
-        fold_change = response_peak / (baseline_rate + 1e-8)
-        
-        # Response consistency
-        response_consistency = 1 - (np.std(post_stimulus) / (mean_response + 1e-8))
-        
-        # Information metric
-        response_entropy = stats.entropy(post_stimulus + 1e-8)
-        
+        rate_changes = response_rates - baseline_rates
+        fold_changes = response_rates / (baseline_rates + 1e-8)
+
+        # Population coding properties
+        active_neurons = np.sum(response_rates > baseline_rates + np.std(baseline_rates))
+        sparsity_index = 1 - (mean_firing_rate**2 / np.mean(self.true_avg_rate["firing_rates"]**2))
+
         return {
-            "baseline_rate": baseline_rate,
-            "peak_rate": response_peak,
-            "mean_response_rate": mean_response,
-            "rate_increase": rate_increase,
-            "fold_change": fold_change,
-            "response_reliability": response_consistency,
-            "response_entropy": response_entropy,
+            "mean_firing_rate": mean_firing_rate.item(),
+            "std_firing_rate": std_firing_rate.item(),
+            "avg_time_vary_rate": np.mean(self.popfirerates).item(),
+            "mean_baseline_rate": np.mean(baseline_rates).item(),
+            "mean_response_rate": np.mean(response_rates).item(),
+            "mean_rate_change": np.mean(rate_changes).item(),
+            "mean_fold_change": np.mean(fold_changes).item(),
+            "active_fraction": (active_neurons / len(self.allspikes_in_window)).item(),
+            "population_sparsity": sparsity_index.item(),
+            "rate_heterogeneity": (std_firing_rate / mean_firing_rate).item(),
+            "response_reliability": (np.sum(rate_changes > 0) / len(self.allspikes_in_window)).item(),
         }
