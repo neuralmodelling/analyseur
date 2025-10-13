ab75e4e7b7dbc74b765276997ce155d67c0783a1
Lungsi <neuralgraphs@gmail.com>
2025-10-13 14:38:12 +0200

    try content organization for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,8 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.append( os.path.dirname( os.getcwd() ) )
+# sys.path.append( os.path.dirname( os.getcwd() ) )
+sys.path.insert(0, os.path.abspath("../src"))
 
 
 # -- Project information -----------------------------------------------------
