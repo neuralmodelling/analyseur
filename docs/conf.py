d6ca9a26c1889e753ba81768a7840cb26202a303
Lungsi <neuralgraphs@gmail.com>
2025-10-14 09:25:15 +0200

    try content organization for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,9 @@
 #
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
 # sys.path.append( os.path.dirname( os.getcwd() ) )
-sys.path.insert(0, os.path.abspath("../src"))
+# sys.path.insert(0, os.path.abspath("../src"))
 
 
 # -- Verify module structure -------------------------------------------------
