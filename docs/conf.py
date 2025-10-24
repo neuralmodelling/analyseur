923e18efbf2f5a7a1b4f7cb687df972b6843a6e4
Lungsi <neuralgraphs@gmail.com>
2025-10-24 09:47:45 +0200

    check math in docstring

--- docs/conf.py
+++ docs/conf.py
@@ -70,7 +70,7 @@ autodoc_mock_imports = [
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # https://www.sphinx-doc.org/en/master/usage/theming.html#builtin-themes
-html_theme = 'classic'
+html_theme = 'scrolls'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
