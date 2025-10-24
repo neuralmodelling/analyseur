13c68b513fb84cbe7bbe57d3b11d6ffd615cd9bf
Lungsi <neuralgraphs@gmail.com>
2025-10-24 10:47:09 +0200

    check math in docstring

--- src/analyseur/cbgt/stats/isi.py
+++ src/analyseur/cbgt/stats/isi.py
@@ -67,9 +67,10 @@ class InterSpikeInterval(object):
         :param all_neurons_isi: Dictionary returned using :py:meth:`.compute`
         :return: dictionary of individual neurons whose values are their respective mean frequencies
 
-        -------
+
         Formula
         -------
+        
         .. table:: Formula
 
         ========================================================================================= ======================================================
