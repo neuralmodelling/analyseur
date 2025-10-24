cc214d6608983774b9b931d0d636333507100fee
Lungsi <neuralgraphs@gmail.com>
2025-10-24 13:20:33 +0200

    restructedtext for ISI

--- src/analyseur/cbgt/stats/isi.py
+++ src/analyseur/cbgt/stats/isi.py
@@ -181,15 +181,15 @@ class InterSpikeInterval(object):
         **Formula**
         
         .. table:: Formula
-        ========================================================================== ======================================================
-          Definitions                                                                Interpretation
-        ========================================================================== ======================================================
-         total neurons, :math:`n_{Nuc}`                                              total number of neurons in the Nucleus
-         neuron index, :math:`i`                                                     i-th neuron in the pool of :math:`n_{Nuc}` neurons
-         mean frequency, :math:`\\overline{f^{(i)}}`                                 mean spiking frequency of i-th neuron
-         :math:`\\vec{F} = [\\overline{f^{(i)}}]_{\\forall{i \\in [1, n_{nuc}]}}`       array of mean frequencies of all (:math:`n_{Nuc}`) neurons
-         grand mean frequency, :math:`\\overline{f}`                                 grand or global mean spiking frequency
-        ========================================================================== ======================================================
+        =================================================================================== ======================================================
+          Definitions                                                                         Interpretation
+        =================================================================================== ======================================================
+         total neurons, :math:`n_{Nuc}`                                                       total number of neurons in the Nucleus
+         neuron index, :math:`i`                                                              i-th neuron in the pool of :math:`n_{Nuc}` neurons
+         mean frequency, :math:`\\overline{f^{(i)}}`                                          mean spiking frequency of i-th neuron
+         :math:`\\vec{F} = \\left[\\overline{f^{(i)}}\\right]_{\\forall{i \\in [1, n_{nuc}]}}`             array of mean frequencies of all (:math:`n_{Nuc}`) neurons
+         grand mean frequency, :math:`\\overline{f}`                                          grand or global mean spiking frequency
+        =================================================================================== ======================================================
 
         NOTE: The array :math:`\\vec{F}` is obtained by calling :py:meth:`.mean_freqs`
 
