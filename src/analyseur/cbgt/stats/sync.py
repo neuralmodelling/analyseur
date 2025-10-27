46befd782740dc17fe9d3b3e7c411e0c67edf56c
Lungsi <neuralgraphs@gmail.com>
2025-10-27 10:05:21 +0100

    restructedtext for sync

--- src/analyseur/cbgt/stats/sync.py
+++ src/analyseur/cbgt/stats/sync.py
@@ -98,36 +98,20 @@ class Synchrony(object):
         .. math::
 
             F = \\overset{\\begin{matrix}t_0 & \\quad\\quad\\quad & t_1 & & & &\\ldots & & & t_{n_{Nuc}}\\end{matrix}}
-        {\\underset{
-            \\begin{matrix}
-            \\quad\\quad\\quad\\quad\\quad\\uparrow & \\quad\\quad\\quad\\quad & \\uparrow & &\\ldots & & & \\uparrow\\\
-             \\quad\\quad\\quad\\quad\\mu_{t_0} & \\quad\\quad\\quad\\quad & \\mu_{t_1} & &\\ldots & & & \\mu_{t_{n_{Nuc}}} & \\rightarrow var_{\\forall{t}}\\\
-       \\end{matrix}}
-       {\\begin{bmatrix}
-         f^{(1)}(t_0) & f^{(1)}(t_1) & \\ldots & f^{(1)}(t_{n_{Nuc}})\\\
-         f^{(2)}(t_0) & f^{(2)}(t_1) & \\ldots & f^{(2)}(t_{n_{Nuc}})\\\
-         \\vdots & \\vdots & \\ldots & \\vdots\\
-         f^{(i)}(t_0) & f^{(i)}(t_1) & \\ldots & f^{(i)}(t_{n_{Nuc}})\\\
-         \\vdots & \\vdots & \\ldots & \\vdots\\\
-         f^{(n_{Nuc})}(t_0) & f^{(n_{Nuc})}(t_1) & \\ldots & f^{(n_{Nuc})}(t_{n_{Nuc}})\\\
-        \\end{bmatrix}
-        }}
-        \\quad\\quad
-            F = \\overset{\\begin{matrix}t_0 & \\quad\\quad\\quad & t_1 & & & &\\ldots & & & t_{n_{Nuc}}\\end{matrix}}
-        {\\underset{
-            \\begin{matrix}
-            \\quad\\quad\\quad\\quad\\quad\\uparrow & \\quad\\quad\\quad\\quad & \\uparrow & &\\ldots & & & \\uparrow\\\
-            \\quad\\quad\\quad\\quad var_{t_0} & \\quad\\quad\\quad\\quad & var_{t_1} & &\\ldots & & & var_{t_{n_{Nuc}}} & \\rightarrow \\mu_{\\forall{t}}\\\
-        \\end{matrix}}
-       {\\begin{bmatrix}
-         f^{(1)}(t_0) & f^{(1)}(t_1) & \\ldots & f^{(1)}(t_{n_{Nuc}})\\\
-         f^{(2)}(t_0) & f^{(2)}(t_1) & \\ldots & f^{(2)}(t_{n_{Nuc}})\\\
-         \\vdots & \\vdots & \\ldots & \\vdots\\\
-         f^{(i)}(t_0) & f^{(i)}(t_1) & \ldots & f^{(i)}(t_{n_{Nuc}})\\\
-         \\vdots & \\vdots & \\ldots & \\vdots\\\
-         f^{(n_{Nuc})}(t_0) & f^{(n_{Nuc})}(t_1) & \\ldots & f^{(n_{Nuc})}(t_{n_{Nuc}})\\\
-        \\end{bmatrix}
-        }}
+            {\\underset{
+                \\begin{matrix}
+                    \\quad\\quad\\quad\\quad\\quad\\uparrow & \\quad\\quad\\quad\\quad & \\uparrow & &\\ldots & & & \\uparrow\\
+                    \\quad\\quad\\quad\\quad\\mu_{t_0} & \\quad\\quad\\quad\\quad & \\mu_{t_1} & &\\ldots & & & \\mu_{t_{n_{Nuc}}} & \\rightarrow var_{\\forall{t}}\\
+                \end{matrix}}
+           {\\begin{bmatrix}
+             f^{(1)}(t_0) & f^{(1)}(t_1) & \\ldots & f^{(1)}(t_{n_{Nuc}})\\
+             f^{(2)}(t_0) & f^{(2)}(t_1) & \\ldots & f^{(2)}(t_{n_{Nuc}})\\
+             \\vdots & \\vdots & \\ldots & \\vdots\\
+             f^{(i)}(t_0) & f^{(i)}(t_1) & \\ldots & f^{(i)}(t_{n_{Nuc}})\\
+             \\vdots & \\vdots & \\ldots & \\vdots\\
+             f^{(n_{Nuc})}(t_0) & f^{(n_{Nuc})}(t_1) & \\ldots & f^{(n_{Nuc})}(t_{n_{Nuc}})\\
+            \\end{bmatrix}
+            }}
 
         Then, synchrony is measured as
 
