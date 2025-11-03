52d688d1701b83731d5251af04cb9629e643ffac
Lungsi <neuralgraphs@gmail.com>
2025-11-03 10:18:49 +0100

    restructured docstring for wavelet

--- src/analyseur/cbgt/stats/wavelet.py
+++ src/analyseur/cbgt/stats/wavelet.py
@@ -260,6 +260,24 @@ class ContinuousWaveletTransform(object):
         True   True   True
         =====  =====  =======
 
+        .. list-table:: Frozen Delights!
+           :widths: 15 10 30
+           :header-rows: 1
+
+           * - Treat
+             - Quantity
+             - Description
+           * - Albatross
+             - 2.99
+             - On a stick!
+           * - Crunchy Frog
+             - 1.49
+             - If we took the bones out, it wouldn't be
+               crunchy, now would it?
+           * - Gannet Ripple
+             - 1.99
+             - On a stick!
+
         .. raw:: html
 
             <hr style="border: 2px solid red; margin: 20px 0;">
