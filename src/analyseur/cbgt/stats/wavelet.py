657d681275f992ee7050fa4189796f0b2dc5994d
Lungsi <neuralgraphs@gmail.com>
2025-11-03 10:48:56 +0100

    restructured docstring for wavelet

--- src/analyseur/cbgt/stats/wavelet.py
+++ src/analyseur/cbgt/stats/wavelet.py
@@ -194,7 +194,7 @@ class ContinuousWaveletTransform(object):
              - Scale vs Voices/Octave
            * - Scales defines the frequencies in the wavelet transform analysis.
              - Voices per octave controls the number of scales between consecutive frequencies (≜ octave)
-           * - :math:`s_a < s_b \\overset{\\frown}{=} f_a < f_b` where scale :math:`s_x` corresponds to frequency :math:`f_x`
+           * - :math:`s_a < s_b \\overset{\\frown}{=} f_a > f_b` where scale :math:`s_x` corresponds to frequency :math:`f_x`
              - In other words, the number of scales between octaves is the voices per octave
 
         .. raw:: html
@@ -239,7 +239,7 @@ class ContinuousWaveletTransform(object):
         **Scale is the dilation/compression factor applied to the wavelet.**
          - Scale vs Frequency
             - Scales defines the frequencies in the wavelet transform analysis.
-            - :math:`s_a < s_b \\overset{\\frown}{=} f_a < f_b` where scale :math:`s_x` corresponds to frequency :math:`f_x`
+            - :math:`s_a < s_b \\overset{\\frown}{=} f_a > f_b` where scale :math:`s_x` corresponds to frequency :math:`f_x`
          - **Scale vs Voices/Octave**
             - Voices per octave controls the number of scales between consecutive frequencies (≜ octave)
             - In other words, the number of scales between octaves is the voices per octave
