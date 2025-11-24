bc851f80c70efc7cc15e9b1adbcdc6fe5ce9c2d3
Lungsi <neuralgraphs@gmail.com>
2025-11-24 09:04:32 +0100

    added rate statistic

--- src/analyseur/cbgt/stats/rate.py
+++ src/analyseur/cbgt/stats/rate.py
@@ -79,7 +79,7 @@ class Rate(object):
                 {\\underset{
                     \\begin{matrix}
                         \\quad\\quad\\downarrow & \\quad\\quad\\quad & \\downarrow & \\quad &\\ldots & & & \\downarrow \n
-                        \\quad\\mu_{t_0} & \\quad\\quad\\quad & \\mu_{t_1} & \\quad &\\ldots & & & \\mu_{t_T} & \\quad
+                        \\quad\\mu_{t_0} & \\quad\\quad\\quad & \\mu_{t_1} & \\quad &\\ldots & & & \\mu_{t_T} & \\quad \\quad
                     \\end{matrix}}
                {\\begin{bmatrix}
                  f^{(1)}(t_0) & f^{(1)}(t_1) & \\ldots & f^{(1)}(t_T) \n
@@ -117,7 +117,7 @@ class Rate(object):
                  \\rightarow \\mu^{(i)} \n
                  \\vdots & \\vdots & \\ldots & \\vdots \n
                  \\rightarow \\mu^{(n_{Nuc})}
-                \\end{matrix
+                \\end{matrix}
 
         .. raw:: html
 
