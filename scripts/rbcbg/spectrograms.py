3d23eed14e200aac780a5fe73e7845f7b6516db8
Lungsi <neuralgraphs@gmail.com>
2026-03-31 13:14:50 +0200

    updated docs for spectrograms

--- scripts/rbcbg/spectrograms.py
+++ scripts/rbcbg/spectrograms.py
@@ -99,54 +99,60 @@ resolution = None
 method = None
 withbands = None
 
-for nucleus in nuclei_list:
-    for i, decay in enumerate(decaylist):
-        str_decay = str(round(decay * 100))
-        filename = nucleus + "_model_9_percent_" + str_decay + ".csv"
-        fullfilepath = rootpath + str(i) + "/" + filename
+def main():
 
-        loadR = LoadRates(fullfilepath)
+    for nucleus in nuclei_list:
+        for i, decay in enumerate(decaylist):
+            str_decay = str(round(decay * 100))
+            filename = nucleus + "_model_9_percent_" + str_decay + ".csv"
+            fullfilepath = rootpath + str(i) + "/" + filename
 
-        t_sec, rates_Hz = loadR.get_rates()
+            loadR = LoadRates(fullfilepath)
 
-        #========= FIGURE-1 =========
-        fig = plt.figure(figsize=(25, 12))
-        gs = fig.add_gridspec(3, 1, height_ratios=[2, 1, 1])
+            t_sec, rates_Hz = loadR.get_rates()
 
-        ax1 = fig.add_subplot(gs[0, 0])  # row 0, col 0
-        ax2 = fig.add_subplot(gs[1, 0], sharex=ax1)  # row 1, col 0
-        ax3 = fig.add_subplot(gs[2, 0])  # row 2, col 0
+            #========= FIGURE-1 =========
+            fig = plt.figure(figsize=(25, 12))
+            gs = fig.add_gridspec(3, 1, height_ratios=[2, 1, 1])
 
-        # ax1 = plot_rate_all_neurons_in_ax(ax1, t_sec, rates_Hz, nucleus=nucleus, window=window)
-        ax1 = plot_mean_rate_in_ax(ax1, t_sec, rates_Hz, nucleus=nucleus, window=window)
-        fig, ax2 = VizPSD.plot_tv_in_axis(fig, ax2, rates_Hz, resolution=resolution, nucleus=nucleus)
-        ax3 = VizPSD.plot_global_in_ax(ax3, rates_Hz, binsz, nucleus=nucleus, resolution=resolution,
-                                       method=method, withbands=withbands)
-        ax3.set_ylim(ax2.get_ylim())
-        ax1.set_xlim(t_sec.min(), t_sec.max())
+            ax1 = fig.add_subplot(gs[0, 0])  # row 0, col 0
+            ax2 = fig.add_subplot(gs[1, 0], sharex=ax1)  # row 1, col 0
+            ax3 = fig.add_subplot(gs[2, 0])  # row 2, col 0
 
-        plt.tight_layout()
+            # ax1 = plot_rate_all_neurons_in_ax(ax1, t_sec, rates_Hz, nucleus=nucleus, window=window)
+            ax1 = plot_mean_rate_in_ax(ax1, t_sec, rates_Hz, nucleus=nucleus, window=window)
+            fig, ax2 = VizPSD.plot_tv_in_axis(fig, ax2, rates_Hz, resolution=resolution, nucleus=nucleus)
+            ax3 = VizPSD.plot_global_in_ax(ax3, rates_Hz, binsz, nucleus=nucleus, resolution=resolution,
+                                        method=method, withbands=withbands)
+            ax3.set_ylim(ax2.get_ylim())
+            ax1.set_xlim(t_sec.min(), t_sec.max())
 
-        # Path(f"psd_rBCBG_{method}").mkdir(parents=True, exist_ok=True)
-        # plt.savefig(f"psd_rBCBG_{method}/PowerSpectrum_of_" + nucleus + "_" + str_decay + "%.png")
-        Path(f"psd_rBCBG").mkdir(parents=True, exist_ok=True)
-        plt.savefig(f"psd_rBCBG/PowerSpectrum_of_" + nucleus + "_" + str_decay + "%.png")
+            plt.tight_layout()
 
-        plt.close()
+            # Path(f"psd_rBCBG_{method}").mkdir(parents=True, exist_ok=True)
+            # plt.savefig(f"psd_rBCBG_{method}/PowerSpectrum_of_" + nucleus + "_" + str_decay + "%.png")
+            Path(f"psd_rBCBG").mkdir(parents=True, exist_ok=True)
+            plt.savefig(f"psd_rBCBG/PowerSpectrum_of_" + nucleus + "_" + str_decay + "%.png")
 
-        #========= FIGURE-2 =========
-        fig = plt.figure(figsize=(25, 12))
-        gs = fig.add_gridspec(2, 1, height_ratios=[1, 1])
+            plt.close()
 
-        ax1 = fig.add_subplot(gs[0, 0])  # row 0, col 0
-        ax2 = fig.add_subplot(gs[1, 0], sharex=ax1)  # row 1, col 0
+            #========= FIGURE-2 =========
+            fig = plt.figure(figsize=(25, 12))
+            gs = fig.add_gridspec(2, 1, height_ratios=[1, 1])
 
-        ax1 = plot_rate_all_neurons_in_ax(ax1, t_sec, rates_Hz, nucleus=nucleus, window=window)
-        ax2 = plot_mean_rate_in_ax(ax2, t_sec, rates_Hz, nucleus=nucleus, window=window)
+            ax1 = fig.add_subplot(gs[0, 0])  # row 0, col 0
+            ax2 = fig.add_subplot(gs[1, 0], sharex=ax1)  # row 1, col 0
 
-        plt.tight_layout()
+            ax1 = plot_rate_all_neurons_in_ax(ax1, t_sec, rates_Hz, nucleus=nucleus, window=window)
+            ax2 = plot_mean_rate_in_ax(ax2, t_sec, rates_Hz, nucleus=nucleus, window=window)
 
-        # plt.savefig(f"psd_rBCBG_{method}/rates_of_" + nucleus + "_" + str_decay + "%.png")
-        plt.savefig(f"psd_rBCBG/rates_of_" + nucleus + "_" + str_decay + "%.png")
+            plt.tight_layout()
 
-        plt.close()
+            # plt.savefig(f"psd_rBCBG_{method}/rates_of_" + nucleus + "_" + str_decay + "%.png")
+            plt.savefig(f"psd_rBCBG/rates_of_" + nucleus + "_" + str_decay + "%.png")
+
+            plt.close()
+
+# RUN THE SCRIPT
+if __name__ == "__main__":
+    main()
